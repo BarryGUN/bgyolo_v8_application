f187e70c773a71d2f0060f3f487dbd51e411a084
BarryGUN <194344131@qq.com>
2023-10-27 16:28:51 +0800

    add

--- detect_train.py
+++ detect_train.py
@@ -4,7 +4,10 @@ if __name__ == '__main__':
     model = YOLO('yolov8s-ms2d-c2repx.yaml')
     model.train(data='vlpd.yaml',
                 epochs=500,
-                project='/runs/detect/train',
-                name='yolov8-ms2-c2repx-v2-sppfcsp-bs16',
+                project='runs/detect/train/s',
+                name='yolov8s-vlpd-ms2d-c2repx-bs16',
                 batch=16,
-                cos_lr=True)
+                cos_lr=True,
+                amp=True
+                )
+
--- detect_val.py
+++ detect_val.py
@@ -3,8 +3,8 @@ from ultralytics import YOLO
 
 
 if __name__ == '__main__':
-    # model = YOLO('runs/detect/train/yolov8n-vlpd-bs16/weights/best.pt')
-    # model.val(project='runs/detect/val', name='yolov8n-vlpd-bs16-fp16', half=False, plots=True)
+    model = YOLO('runs/detect/train/n/yolov8n-vlpd-bs16/weights/best.pt')
+    model.val(project='runs/detect/val/n', name='yolov8n-vlpd-bs16', half=True, plots=True)
 
-    model = YOLO('yolov8n.pt')
-    model.val(project='runs/detect/val', name='yolov8n-coco', half=False, plots=True, data='coco.yaml')
+    # model = YOLO('yolov8n.pt')
+    # model.val(project='runs/detect/val', name='yolov8n-coco', half=False, plots=True, data='coco.yaml')
