c54b263d2feb8a39edfbddac5a899da8fa751e8a
BarryGUN <10863370+barryGUN@user.noreply.gitee.com>
2023-10-28 10:14:54 +0800

    ms2

--- detect_train.py
+++ detect_train.py
@@ -1,13 +1,17 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('yolov8s-ms2d-c2repx.yaml')
+    model = YOLO('yolov8m-ms2d-c2repx.yaml')
+    # model = YOLO('yolov8m.yaml')
     model.train(data='vlpd.yaml',
                 epochs=500,
-                project='runs/detect/train/s',
+                project='runs/detect/train/m',
                 name='yolov8s-vlpd-ms2d-c2repx-bs16',
                 batch=16,
                 cos_lr=True,
                 amp=True
                 )
+
+
+
 
