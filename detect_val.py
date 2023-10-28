73c4f2cabc560b8530ff9145e1250380fb5f9004
BarryGUN <194344131@qq.com>
2023-10-29 01:08:35 +0800

    add

--- detect_val.py
+++ detect_val.py
@@ -3,8 +3,8 @@ from ultralytics import YOLO
 
 
 if __name__ == '__main__':
-    model = YOLO('runs/detect/train/n/yolov8n-vlpd-bs16/weights/best.pt')
-    model.val(project='runs/detect/val/n', name='yolov8n-vlpd-bs16', half=True, plots=True)
+    model = YOLO('runs/detect/train/m/yolov8m-ms2d-c2repx-vlpd-bs16/weights/best.pt')
+    model.val(project='runs/detect/val/m', name='yolov8m-ms2d-c2repx-vlpd-bs16', half=True, plots=True)
 
     # model = YOLO('yolov8n.pt')
     # model.val(project='runs/detect/val', name='yolov8n-coco', half=False, plots=True, data='coco.yaml')
