e95f2143ef75c4797dd847dce5111cc6e0cdc2e4
BarryGUN <10863370+barryGUN@user.noreply.gitee.com>
2023-11-14 22:06:59 +0800

    densefpn

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -665,3 +665,27 @@ class BiConcat(nn.Module):
         for i in range(self.n):
             x[i] = weight[i] * x[i]
         return torch.cat(x, self.d)
+
+
+class C2fBi(nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+        self.bcat = BiConcat((2 + n), dimension=1)
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(self.bcat(y))
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(self.bcat(y))
--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -12,7 +12,7 @@ from ultralytics.nn.modules import (AIFI, C1, C2, C3, C3TR, SPP, SPPF, Bottlenec
                                     Focus, GhostBottleneck, GhostConv, HGBlock, HGStem, Pose, RepC3, RepConv,
                                     RTDETRDecoder, Segment)
 from ultralytics.nn.modules.block import MS2, MS2b, C2RepX, SplitMP, SPPFCSP, MS2d, MS2e, C2d, C2sc, C2RepXc, BiFuse, \
-    BiConcat
+    BiConcat, C2fBi
 from ultralytics.nn.modules.block_controlled_trial import C2RepXCCAB, C2RepXCBCBA
 from ultralytics.nn.modules.conv import RepXConv
 from ultralytics.nn.modules.repxconv_controlled_trial import RepXConvCCAB, RepXConvCBCBA
@@ -659,7 +659,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
         n = n_ = max(round(n * depth), 1) if n > 1 else n  # depth gain
         if m in (Classify, Conv, ConvTranspose, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF, SPPFCSP, DWConv, Focus,
                  BottleneckCSP, C1, C2, C2f, C3, C3TR, C3Ghost, nn.ConvTranspose2d, DWConvTranspose2d, C3x, RepC3,
-                 MS2, MS2b, C2RepX, SplitMP, MS2d, MS2e, C2RepXCCAB, C2RepXCBCBA, C2d, C2sc, C2RepXc,BiFuse):
+                 MS2, MS2b, C2RepX, SplitMP, MS2d, MS2e, C2RepXCCAB, C2RepXCBCBA, C2d, C2sc, C2RepXc, BiFuse, C2fBi):
             if m is BiFuse:
                 c_list = list(ch[i] for i in f)
                 c1, c2 = c_list, args[0]
@@ -673,7 +673,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
 
                 args = [c1, c2, *args[1:]]
                 if m in (BottleneckCSP, C1, C2, C2f, C3, C3TR, C3Ghost, C3x, RepC3, MS2, MS2b, C2RepX,
-                         MS2d, MS2e, C2RepXCCAB, C2RepXCBCBA, C2d, C2sc, C2RepXc):
+                         MS2d, MS2e, C2RepXCCAB, C2RepXCBCBA, C2d, C2sc, C2RepXc, C2fBi):
                     args.insert(2, n)  # number of repeats
                     n = 1
 
