afc112d90eee145b01b3dd8b603a912d198640c1
BarryGUN <10863370+barryGUN@user.noreply.gitee.com>
2023-11-15 19:42:09 +0800

    densefpn

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -707,3 +707,23 @@ class C2fBi(nn.Module):
         y = list(self.cv1(x).split((self.c, self.c), 1))
         y.extend(m(y[-1]) for m in self.m)
         return self.cv2(self.bcat(y))
+
+
+class C2x(nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, self.c, 1, 1)
+        self.cv2 = Conv(c1, self.c, 1, 1)
+        self.cv3 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        # y = list(self.cv1(x).chunk(2, 1))
+        y = [self.cv1(x), self.cv2(x)]
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv3(torch.cat(y, dim=1))
+
