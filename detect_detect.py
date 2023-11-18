b9f16ba5d85a9c27c7ea9bf7c25c4279a6c44ba1
BarryGUN <10863370+barryGUN@user.noreply.gitee.com>
2023-11-18 15:28:33 +0800

    densefpn

--- detect_detect.py
+++ detect_detect.py
@@ -1,14 +1,20 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('yolov8n-bdd100k.pt')
+    model = YOLO('w/dense/best.pt')
+    # model = YOLO('w/v8/best.pt')
     # results = model(r'C:\Users\lenovo\Desktop\car-drive\01.mp4', device='0', half=False, show=True, conf=0.4, iou=0.6)
 
 
-    results = model('demo/001.jpg',
+    results = model('demo/004.jpg',
                     device='0',
                     half=False,
-                    show=True,
-                    conf=0.4,
+                    show=False,
+                    save=True,
+                    conf=0.22,
+                    # conf=0.208,
                     iou=0.6,
-                    visualize=True)
+                    project='runs/val/n/',
+                    name='densefpn',
+                    # name='v8',
+                    plots=True)
--- detect_train.py
+++ detect_train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # model = YOLO('yolov8m-ms2d-c2repx.yaml')
-    model = YOLO('bdd100k/yolov8n-bibncat.yaml')
+    model = YOLO('bdd100k/yolov8n-densefpn-mf.yaml')
     # model = YOLO('yolov8n.yaml')
     model.train(data='coco128.yaml',
                 epochs=500,
@@ -10,7 +10,8 @@ if __name__ == '__main__':
                 name='yolov8s-vlpd-ms2e-c2repx-bs16-v2',
                 batch=2,
                 cos_lr=True,
-                amp=True
+                amp=True,
+                wiou=True
                 )
 
 
