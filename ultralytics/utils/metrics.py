4ea3ab408989cbcc7f6aef8930ae46dabbfe6deb
BarryGUN <10863370+barryGUN@user.noreply.gitee.com>
2023-11-29 10:30:17 +0800

    densefpn

--- ultralytics/utils/metrics.py
+++ ultralytics/utils/metrics.py
@@ -146,71 +146,71 @@ def bbox_iou(box1,
     # Union Area
     union = w1 * h1 + w2 * h2 - inter + eps
 
-    # # IoU
-    # iou = inter / union
-    # if CIoU or DIoU or GIoU:
-    #     cw = b1_x2.maximum(b2_x2) - b1_x1.minimum(b2_x1)  # convex (smallest enclosing box) width
-    #     ch = b1_y2.maximum(b2_y2) - b1_y1.minimum(b2_y1)  # convex height
-    #     if CIoU or DIoU:  # Distance or Complete IoU https://arxiv.org/abs/1911.08287v1
-    #         c2 = cw ** 2 + ch ** 2 + eps  # convex diagonal squared
-    #         rho2 = ((b2_x1 + b2_x2 - b1_x1 - b1_x2) ** 2 + (b2_y1 + b2_y2 - b1_y1 - b1_y2) ** 2) / 4  # center dist ** 2
-    #         if CIoU:  # https://github.com/Zzh-tju/DIoU-SSD-pytorch/blob/master/utils/box/box_utils.py#L47
-    #             v = (4 / math.pi ** 2) * (torch.atan(w2 / h2) - torch.atan(w1 / h1)).pow(2)
-    #             with torch.no_grad():
-    #                 alpha = v / (v - iou + (1 + eps))
-    #             return iou - (rho2 / c2 + v * alpha)  # CIoU
-    #         return iou - rho2 / c2  # DIoU
-    #     c_area = cw * ch + eps  # convex area
-    #     return iou - (c_area - union) / c_area  # GIoU https://arxiv.org/pdf/1902.09630.pdf
-
     # IoU
-    iou = torch.pow(inter / union + eps, alpha_value) if alphaIoU else inter / union
-
-    if CIoU or DIoU or GIoU or WIoU or EIoU:
+    iou = inter / union
+    if CIoU or DIoU or GIoU:
         cw = b1_x2.maximum(b2_x2) - b1_x1.minimum(b2_x1)  # convex (smallest enclosing box) width
         ch = b1_y2.maximum(b2_y2) - b1_y1.minimum(b2_y1)  # convex height
-        if CIoU or DIoU or WIoU or EIoU:  # Distance or Complete IoU https://arxiv.org/abs/1911.08287v1
-            if alphaIoU:
-                c2 = (cw ** 2 + ch ** 2) ** alpha_value + eps
-            else:
-                c2 = cw ** 2 + ch ** 2 + eps  # convex diagonal squared
-            if alphaIoU:
-                rho2 = (((b2_x1 + b2_x2 - b1_x1 - b1_x2) ** 2 + (b2_y1 + b2_y2 - b1_y1 - b1_y2) ** 2) / 4) ** alpha_value
-            else:
-                rho2 = ((b2_x1 + b2_x2 - b1_x1 - b1_x2) ** 2 + (b2_y1 + b2_y2 - b1_y1 - b1_y2) ** 2) / 4  # center dist ** 2
-
-
-
+        if CIoU or DIoU:  # Distance or Complete IoU https://arxiv.org/abs/1911.08287v1
+            c2 = cw ** 2 + ch ** 2 + eps  # convex diagonal squared
+            rho2 = ((b2_x1 + b2_x2 - b1_x1 - b1_x2) ** 2 + (b2_y1 + b2_y2 - b1_y1 - b1_y2) ** 2) / 4  # center dist ** 2
             if CIoU:  # https://github.com/Zzh-tju/DIoU-SSD-pytorch/blob/master/utils/box/box_utils.py#L47
                 v = (4 / math.pi ** 2) * (torch.atan(w2 / h2) - torch.atan(w1 / h1)).pow(2)
                 with torch.no_grad():
-                    alpha = v / ((1 + eps) - inter / union + v) if alphaIoU else v / (v - iou + (1 + eps))
-                if alphaIoU:
-                    return iou - (rho2 / c2 + torch.pow(v * alpha + eps, alpha_value))
-                else:
-                    return iou - (rho2 / c2 + v * alpha)  # CIoU
-
-            if EIoU:
-                rho_w2 = ((b2_x2 - b2_x1) - (b1_x2 - b1_x1)) ** (alpha_value * 2) if alphaIoU else ((b2_x2 - b2_x1) - (b1_x2 - b1_x1)) ** 2
-                rho_h2 = ((b2_y2 - b2_y1) - (b1_y2 - b1_y1)) ** (alpha_value * 2) if alphaIoU else ((b2_y2 - b2_y1) - (b1_y2 - b1_y1)) ** 2
-                cw2 = torch.pow(cw ** 2 + eps,  alpha_value) if alphaIoU else cw ** 2 + eps
-                ch2 = torch.pow(ch ** 2 + eps,  alpha_value) if alphaIoU else ch ** 2 + eps
-                return iou - (rho2 / c2 + rho_w2 / cw2 + rho_h2 / ch2)
-
-            if WIoU:
-                if alphaIoU:
-                    raise NotImplementedError('WIoU do not support alpha power')
-                self = WIoU_Scale(iou=1 - iou,
-                                  batch_size=WIoUDict['batch_size'],
-                                  epoch=WIoUDict['epoch'],
-                                  delta=WIoUDict['delta'],
-                                  gamma=WIoUDict['gamma'])
-                wiou_1 = (1 - iou) * torch.exp(rho2 / c2.detach())
-                return getattr(WIoU_Scale, '_scaled_loss')(self), wiou_1, iou
-
+                    alpha = v / (v - iou + (1 + eps))
+                return iou - (rho2 / c2 + v * alpha)  # CIoU
             return iou - rho2 / c2  # DIoU
         c_area = cw * ch + eps  # convex area
         return iou - (c_area - union) / c_area  # GIoU https://arxiv.org/pdf/1902.09630.pdf
+
+    # # IoU
+    # iou = torch.pow(inter / union + eps, alpha_value) if alphaIoU else inter / union
+    #
+    # if CIoU or DIoU or GIoU or WIoU or EIoU:
+    #     cw = b1_x2.maximum(b2_x2) - b1_x1.minimum(b2_x1)  # convex (smallest enclosing box) width
+    #     ch = b1_y2.maximum(b2_y2) - b1_y1.minimum(b2_y1)  # convex height
+    #     if CIoU or DIoU or WIoU or EIoU:  # Distance or Complete IoU https://arxiv.org/abs/1911.08287v1
+    #         if alphaIoU:
+    #             c2 = (cw ** 2 + ch ** 2) ** alpha_value + eps
+    #         else:
+    #             c2 = cw ** 2 + ch ** 2 + eps  # convex diagonal squared
+    #         if alphaIoU:
+    #             rho2 = (((b2_x1 + b2_x2 - b1_x1 - b1_x2) ** 2 + (b2_y1 + b2_y2 - b1_y1 - b1_y2) ** 2) / 4) ** alpha_value
+    #         else:
+    #             rho2 = ((b2_x1 + b2_x2 - b1_x1 - b1_x2) ** 2 + (b2_y1 + b2_y2 - b1_y1 - b1_y2) ** 2) / 4  # center dist ** 2
+    #
+    #
+    #
+    #         if CIoU:  # https://github.com/Zzh-tju/DIoU-SSD-pytorch/blob/master/utils/box/box_utils.py#L47
+    #             v = (4 / math.pi ** 2) * (torch.atan(w2 / h2) - torch.atan(w1 / h1)).pow(2)
+    #             with torch.no_grad():
+    #                 alpha = v / ((1 + eps) - inter / union + v) if alphaIoU else v / (v - iou + (1 + eps))
+    #             if alphaIoU:
+    #                 return iou - (rho2 / c2 + torch.pow(v * alpha + eps, alpha_value))
+    #             else:
+    #                 return iou - (rho2 / c2 + v * alpha)  # CIoU
+    #
+    #         if EIoU:
+    #             rho_w2 = ((b2_x2 - b2_x1) - (b1_x2 - b1_x1)) ** (alpha_value * 2) if alphaIoU else ((b2_x2 - b2_x1) - (b1_x2 - b1_x1)) ** 2
+    #             rho_h2 = ((b2_y2 - b2_y1) - (b1_y2 - b1_y1)) ** (alpha_value * 2) if alphaIoU else ((b2_y2 - b2_y1) - (b1_y2 - b1_y1)) ** 2
+    #             cw2 = torch.pow(cw ** 2 + eps,  alpha_value) if alphaIoU else cw ** 2 + eps
+    #             ch2 = torch.pow(ch ** 2 + eps,  alpha_value) if alphaIoU else ch ** 2 + eps
+    #             return iou - (rho2 / c2 + rho_w2 / cw2 + rho_h2 / ch2)
+    #
+    #         if WIoU:
+    #             if alphaIoU:
+    #                 raise NotImplementedError('WIoU do not support alpha power')
+    #             self = WIoU_Scale(iou=1 - iou,
+    #                               batch_size=WIoUDict['batch_size'],
+    #                               epoch=WIoUDict['epoch'],
+    #                               delta=WIoUDict['delta'],
+    #                               gamma=WIoUDict['gamma'])
+    #             wiou_1 = (1 - iou) * torch.exp(rho2 / c2.detach())
+    #             return getattr(WIoU_Scale, '_scaled_loss')(self), wiou_1, iou
+    #
+    #         return iou - rho2 / c2  # DIoU
+    #     c_area = cw * ch + eps  # convex area
+    #     return iou - (c_area - union) / c_area  # GIoU https://arxiv.org/pdf/1902.09630.pdf
 
     return iou  # IoU
 
