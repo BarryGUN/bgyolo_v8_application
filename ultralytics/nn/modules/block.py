27e12d8c85f2f7871243b0dadb9ce9604b4863cf
BarryGUN <10863370+barryGUN@user.noreply.gitee.com>
2023-12-14 09:48:07 +0800

    densefpn

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -776,14 +776,14 @@ class C2el(nn.Module):
         self.c = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, 2 * self.c, 1, 1)
         self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
-        self.attention = SpatialAttention(kernel_size=3)  # or SpatialAttention()
+        # self.attention = SpatialAttention(kernel_size=3)  # or SpatialAttention()
         self.m = nn.ModuleList(Conv(self.c, self.c, k=3, s=1, g=g) for _ in range(n))
 
     def forward(self, x):
         """Forward pass through C2f layer."""
         y = list(self.cv1(x).chunk(2, 1))
         y.extend(m(y[-1]) for m in self.m)
-        return self.cv2(self.attention(torch.cat(y, 1)))
+        return self.cv2(torch.cat(y, 1))
 
     def forward_split(self, x):
         """Forward pass using split() instead of chunk()."""
