021479b680e8b9c54bb8cb9e604688a54a6efe6f
BarryGUN <10863370+barryGUN@user.noreply.gitee.com>
2023-12-27 11:46:43 +0800

    densefpn

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -656,19 +656,31 @@ class TranConcat(nn.Module):
 
 
 class TranQKVConcat(nn.Module):
-    def __init__(self, dim, dimension=1, eps=1e-5):
+    def __init__(self, dim, dimension=1, eps=1e-7):
         super(TranQKVConcat, self).__init__()
         self.d = dimension
         self.k = DWConv(dim, dim, k=3, s=1)
-        self.q = nn.Sequential(*(DWConv(dim, dim, k=3, s=1) for _ in range(2)))
+        self.q = DWConv(dim, dim, k=3, s=1)
         self.v = nn.Identity()
         self.linear = Conv(dim, dim, k=1, s=1)
-        self.bn = nn.BatchNorm2d(dim)
+        # self.bconv = DWConv(dim, dim, k=3, s=1)
+        self.ln_1 = nn.LayerNorm(dim, eps=eps)
+        self.ln_2 = nn.LayerNorm(dim, eps=eps)
+        # self.bn = nn.BatchNorm2d(dim)
         # self.act = nn.SiLU()
         self.eps = eps
 
+    def __laynorm__(self, laynorm, x):
+        x = x.permute(0, 2, 3, 1)  # (N, C, H, W) -> (N, H, W, C)
+        x = laynorm(x)
+        return x.permute(0, 3, 1, 2)  # (N, H, W, C) -> (N, C, H, W)
+
     def forward(self, x):
         x = torch.cat(x, self.d)
+        v = self.v(x)
+        x = self.__laynorm__(self.ln_1, self.k(x) * self.q(x))
+        x = self.__laynorm__(self.ln_2, x * v)
+
         # return x + self.linear(self.bn(
         #     (self.q(x) + self.eps) * (self.k(x) + self.eps)
         # ) * (self.v(x) + self.eps))
@@ -676,10 +688,8 @@ class TranQKVConcat(nn.Module):
         #     (self.q(x) + self.eps) * (self.k(x) + self.eps)
         # ) * (self.v(x) + self.eps))
 
-        y = self.linear(self.bn(
-            (torch.matmul(self.q(x), self.k(x)))
-        ) * (self.v(x) + self.eps))
-        return y
+
+        return x
         # return self.linear(self.act(self.bn(
         #     (self.q(x) + self.eps) * (self.k(x) + self.eps)
         # )) * (self.v(x) + self.eps))
