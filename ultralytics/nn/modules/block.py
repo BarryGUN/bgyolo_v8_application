265d47c7531ad3ccc3f3bd2ba040c4d191153266
BarryGUN <10863370+barryGUN@user.noreply.gitee.com>
2024-01-02 00:24:31 +0800

    densefpn

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -532,8 +532,8 @@ class TranQKVConcat(nn.Module):
         self.k = nn.Sequential(*(DWConv(dim, dim, k=3, s=1) for _ in range(2)))
         self.v = nn.Identity()
         self.linear = Conv(dim, dim, k=1, s=1)
-        # self.gn = nn.GroupNorm(num_groups=dim // 32, num_channels=dim)
-        self.gn = nn.GroupNorm(num_groups=dim // 16, num_channels=dim)
+        self.gn = nn.GroupNorm(num_groups=dim // 32, num_channels=dim)
+        # self.gn = nn.GroupNorm(num_groups=dim // 16, num_channels=dim)
         # self.gn = nn.GroupNorm(num_groups=dim // 8, num_channels=dim)
         # self.gn = nn.GroupNorm(num_groups=dim // 2, num_channels=dim)
         # self.innorm = nn.InstanceNorm2d(dim)
@@ -549,12 +549,12 @@ class TranQKVConcat(nn.Module):
         #     self.innorm(self.k(x) * self.q(x)) * self.v(x)
         # )
 
-        # return self.linear(
-        #     self.gn(self.k(x) * self.q(x)) * self.v(x)
-        # )
         return self.linear(
-            self.gn((self.k(x) + self.eps) * (self.q(x) + self.eps)) * (self.v(x) + self.eps)
+            self.gn(self.k(x) * self.q(x)) * self.v(x)
         )
+        # return self.linear(
+        #     self.gn((self.k(x) + self.eps) * (self.q(x) + self.eps)) * (self.v(x) + self.eps)
+        # )
 
 
         # return x + self.linear(self.bn(
